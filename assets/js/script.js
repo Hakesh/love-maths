064f40a390668f36e6c4fc3916c14212592004c7
Hakesh <scout816@gmail.com>
2022-11-22 21:37:48 +0000

    Added code for subtraction part of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,7 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     }
     else if (operator === "-") {
-
+        return [operand1 - operand2, "subtract"]
     }
     else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
@@ -111,10 +111,10 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
     
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "-";
 
 }
