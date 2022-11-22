dafdb00fd81c34d24e9b5f2e2df0a4b11d86be25
Hakesh <scout816@gmail.com>
2022-11-22 21:29:34 +0000

    Added code for multipliation and parts for other operators

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,12 @@ function runGame(gameType) {
     if(gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     }
+    else if(gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    }
+    else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }
     else {
         alert(`Unkown game type: ${gameType}`);
         throw `Unkown game type: ${gameType}. Aborting!`;
@@ -69,6 +75,12 @@ function calculateCorrectAnswer() {
     if(operator === "+") {
         return [operand1 + operand2, "addition"];
     }
+    else if (operator === "-") {
+
+    }
+    else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    }
     else {
         alert(`error: wrong ${operator}`);
         throw `error: wrong ${operator}. Aborting!`;
@@ -101,11 +113,17 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion() {
     
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "-";
+
 }
 
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
 
 
